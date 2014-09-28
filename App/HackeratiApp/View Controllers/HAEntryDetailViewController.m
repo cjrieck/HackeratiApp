9814ad3d6637eded1092d3c2049d1d8e51d888b1
Clayton Rieck <cjrieck123@gmail.com>
2014-09-28 11:09:35 -0400

    add sharing features

--- App/HackeratiApp/View Controllers/HAEntryDetailViewController.m
+++ App/HackeratiApp/View Controllers/HAEntryDetailViewController.m
@@ -30,10 +30,44 @@
 {
     self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.navigationController.toolbarHidden = NO;
+     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Favorite"
+                                                                                                    style:UIBarButtonItemStylePlain
+                                                                                                   target:self
+                                                                                                   action:@selector(favorite)];
+    
+    UIBarButtonItem *shareButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction
+                                                                                 target:self
+                                                                                 action:@selector(share)];
+    
+    UIBarButtonItem *flexSpacing = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                                                 target:nil
+                                                                                 action:nil];
+    
+    self.toolbarItems = @[shareButton, flexSpacing];
+    
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+- (void)share
+{
+    // !!!: Sharing options (aside from Apple actions and custom actions) will only show up on a device
+    NSURL *shareURL = self.entry.storeLink;
+    UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[shareURL] applicationActivities:nil];
+    // Following spec for only copy, email and Twitter sharing capabilities. Remove this line to enable all possible sharing options
+    activityViewController.excludedActivityTypes = @[UIActivityTypeMessage, UIActivityTypePostToFacebook];
+    activityViewController.popoverPresentationController.sourceView = self.view;
+    activityViewController.popoverPresentationController.sourceRect = self.view.bounds;
+    [self.navigationController presentViewController:activityViewController animated:YES completion:nil];
+}
+
+- (void)favorite
+{
+    // TODO: Save to core data
 }
 
 @end
